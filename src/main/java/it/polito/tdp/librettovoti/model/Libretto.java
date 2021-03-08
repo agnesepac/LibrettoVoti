415e717926a30f318787b00a55eb0554a0d57fbe
agnes <agnes@LAPTOP-8OO6TLMC.station>
2021-03-08 11:20:27 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -24,4 +24,49 @@ public class Libretto {
 		return s;
 	}
 
+	/*
+	public void stampaVotiUguali (int punteggio) {
+		//il libretto stampa da solo i voti
+		
+		System.out.println();	}
+	
+	public String votiUguali (int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli -> MEGLIO QUESTO METODO DEL PRECEDENTE
+		// -> Solo il NOME?
+		// -> Tutto il voto.toString();
+		
+	}  */
+	
+	public List <Voto> listaVotiUguali (int punteggio){
+		 //restituisce solo i voti uguali al criterio
+		
+		//il difetto di questo metodo è che espone all'esterno il tipo di dato utilizzato
+		//per memorizzare i dati, quindi se decidessi di organizzare i dati in modo diverso
+		//dovrei modificare il metodo
+		
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v); //lo stesso v sarà contenuto in 2 liste DIVERSE
+			}
+		}
+		
+		return risultato;
+	
+	}
+	
+	
+	 public Libretto votiUguali(int punteggio) {
+		//restituisco un libretto che contenga solo i corsi con voti superiori a 25
+
+	 Libretto risultato = new Libretto();
+	 for(Voto v: this.voti) {
+		 if(v.getVoto()==punteggio) {
+			risultato.add(v);
+		 }	 
+	 }
+	 
+	 return risultato;
+	 } 
 }
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	
@@ -9,18 +10,23 @@ public class TestLibretto {
 	System.out.println("Test metodi di Libretto");
 	Libretto libretto = new Libretto(); //creo libretto
 	
-	
 	//aggiungo voti nel libretto
 	
 	Voto voto1= new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
-	
 	libretto.add(voto1);
 	libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 	libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9, 15)));
+	libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9, 15)));
 	
 	//Stampo contenuto libretto
 	
 	System.out.println(libretto); 
+	
+	List <Voto> venticinque = libretto.listaVotiUguali(25); //creo una lista
+	System.out.println(venticinque); 
+	
+	Libretto librettoventicinque = libretto.votiUguali(25); //creo un libretto, un oggetto che sa come gestire i propri dati
+	System.out.println(librettoventicinque); //usa il toString di Libretto
 }
 
 }
